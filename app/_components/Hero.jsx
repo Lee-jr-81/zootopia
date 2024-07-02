1eb5d7ec1886cd6d62e48c64f774ccffc65b63b2
Lee-jr-81 <129674277+Lee-jr-81@users.noreply.github.com>
2024-07-02 19:25:23 +0100

    Changed the font family

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -7,7 +7,7 @@ function Hero() {
     <section className="text-gray-600 body-font bg-[#fff8eb] dark:bg-slate-900">
       <div className="container flex md:w-2/3 md:mx-auto md:py-10 md:flex-row flex-col items-center">
         <div className="lg:flex-grow mt-5 md:mt-0   md:w-1.5/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-8 md:mb-0 items-center text-center">
-          <h1 className="text-2xl font-extrabold leading-9 tracking-tight mb-3 text-[#652b19] dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-normal">
+          <h1 className="text-3xl font-customFont leading-9 tracking-tight mb-3 text-[#652b19] dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-normal">
             Find your friend
           </h1>
           <p className="mb-8 md:pl-0  pl-2 pr-2 leading-relaxed text-[#975f4e] dark:text-gray-300">
